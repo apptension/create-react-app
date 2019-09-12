20fdad650a61532f874cdcb02626619563aaf5b5
Michał Przyszczypkowski <mprzyszczypkowski@apptension.com>
2019-09-12 14:29:47 +0200

    Fix typo

--- packages/react-scripts/template/src/theme/global.js
+++ packages/react-scripts/template/src/theme/global.js
@@ -1,10 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 import { Font } from './theme.constants';
+import { font } from './theme';
 
 export const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
-    font-family: ${Font.PRIMARY};
+    font-family: ${font[Font.PRIMARY]};
   }
   *, *:before, *:after {
     box-sizing: inherit;
