405c1ac0ca005f70d0a0a41751a99f7c2e604bdc
Patryk Ziemkowski <pziemkowski@apptension.com>
2020-10-30 08:51:29 +0100

    Push latest react-scripts

--- packages/react-scripts/template/fixtures/store.js
+++
@@ -1,7 +0,0 @@
-export const locales = {
-  language: 'en',
-};
-
-export const store = {
-  locales,
-};
--- packages/react-scripts/template/src/shared/components/button/button.constants.js
+++
@@ -1,4 +0,0 @@
-export const BUTTON_TYPE_PRIMARY = 'primary';
-export const BUTTON_TYPE_SECONDARY = 'secondary';
-
-export const BUTTON_TYPES = [BUTTON_TYPE_PRIMARY, BUTTON_TYPE_SECONDARY];
